a3f3b33de1c591e7bbe2c8425f7a2621b602fa28
Olenka <olena@gavatyuk.org.ua>
2017-02-22 09:10:21 +0100

    changed after comments

--- initDB.sql
+++ initDB.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE summary_table_dev;
+commit;
+
 CREATE TABLE developers(
 ID   INT               NOT NULL AUTO_INCREMENT,
 NAME VARCHAR (25)     NOT NULL,
@@ -10,7 +13,6 @@ commit;
 CREATE TABLE skills(
 ID   INT NOT NULL AUTO_INCREMENT,
 skill VARCHAR (100) NOT NULL,
-DEVELOPER_ID INT REFERENCES developers (ID) NOT NULL,
 PRIMARY KEY (ID)
 );
 commit;
@@ -18,16 +20,13 @@ commit;
 CREATE TABLE companies(
 ID   INT NOT NULL AUTO_INCREMENT,
 company VARCHAR (120) NOT NULL,
-DEVELOPER_ID INT REFERENCES developers (ID) NOT NULL,
-PROJECT_ID INT REFERENCES projects (ID) NOT NULL,
 PRIMARY KEY (ID)
 );
 commit;
 
 CREATE TABLE customers(
 ID   INT NOT NULL AUTO_INCREMENT,
-skill VARCHAR (100) NOT NULL,
-DEVELOPER_ID INT REFERENCES companies (ID) NOT NULL,
+customer VARCHAR (100) NOT NULL,
 PRIMARY KEY (ID)
 );
 commit;
@@ -35,7 +34,10 @@ commit;
 CREATE TABLE projects(
 ID   INT NOT NULL AUTO_INCREMENT,
 project VARCHAR (100) NOT NULL,
-proj_customers int REFERENCES customers (ID),
+proj_customers int ,
+proj_company int,
+company_FK foreign key (proj_company) references companies(id);
+customer_FK foreign key (proj_customers) references customers(id);
 PRIMARY KEY (ID)
 );
 commit;
@@ -43,7 +45,9 @@ commit;
 CREATE TABLE dev_skills(
 ID   INT NOT NULL AUTO_INCREMENT,
 DEVELOPER_ID INT  NOT NULL,
-SKILL_ID  NOT NULL,
+SKILL_ID INT  NOT NULL,
+developer_FK foreign key (DEVELOPER_ID) references DEVELOPERS(id),
+skills_FK foreign key (SKILL_ID) references skills(id),
 PRIMARY KEY (ID)
 );
 commit;
@@ -52,24 +56,11 @@ CREATE TABLE dev_project(
 ID   INT NOT NULL AUTO_INCREMENT,
 DEVELOPER_ID INT  NOT NULL,
 PROJECT_ID INT  NOT NULL,
-develop_FK foreign key (DEVELOPER_ID) references developers(id)
+develop_FK foreign key (DEVELOPER_ID) references developers(id),
+project_FK foreign key (PROJECT_ID) references projects(id),
 PRIMARY KEY (ID)
 );
 commit;
 
 Alter Table developers add constraint dev_company_FK foreign key (dev_company) references companies(id);
-commit;
-
-Alter Table dev_project add constraint develop_FK foreign key (DEVELOPER_ID) references developers(id);
-commit;
-
-Alter Table dev_project add constraint project_FK foreign key (PROJECT_ID) references projects(id);
-commit;
-
-Alter Table dev_skills add constraint developer_FK foreign key (DEVELOPER_ID) references DEVELOPERS(id);
-Alter Table dev_skills add constraint skills_FK foreign key (SKILL_ID) references skills(id);
-commit;
-
-Alter Table projects add constraint company_FK foreign key (proj_company) references companies(id);
-Alter Table projects add constraint customer_FK foreign key (proj_customers) references customers(id);
 commit;
